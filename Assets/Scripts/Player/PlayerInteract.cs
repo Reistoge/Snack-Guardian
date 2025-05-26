a7a3ac34aa0d8a71996c65bd6eefce3734425a8b
Reistoge <64452194+Reistoge@users.noreply.github.com>
2025-05-26 13:19:07 -0400

    minor

--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -44,7 +44,6 @@ public class PlayerInteract : MonoBehaviour, IInteractor
                 {
                     player.takeDamage(effect.amount);
                     player.applyEffect(effect);
-
                 }
                 break;
             case EffectType.Nothing:
