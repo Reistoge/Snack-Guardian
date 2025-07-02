e00eee721be93e28feefc894e662dec77711ee9e
Reistoge <64452194+Reistoge@users.noreply.github.com>
2025-07-02 03:09:40 -0400

    arreglos visuales
    Agregados:
    > Juagador puede ahora copiar al clipboard cuando hace click en los textos de los jugadores activos
    > Se reordeno la UI y ahora se muestra el id
    > Ahora se muestran colores correctamente en los textos y son guardados en constantes
    > chat privado pero solo como UI falta implementar el codigo

---
+++ Assets/UIPrivateMessage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPrivateMessage : MonoBehaviour
+{
+    InputField inputField;
+    Button sendButton;
+    TextMeshProUGUI chatDisplay;
+
+    
+
+}
